5c71cc78dea5dd23532eba6ab240248d5f4d1f57
mbaluev <mikhail.baluev@gmail.com>
2026-05-01 10:32:42 +0200

    fix lists

--- web/src/components/debug/list-load/list.tsx
+++ web/src/components/debug/list-load/list.tsx
@@ -3,29 +3,16 @@
 import { useMemo } from 'react';
 import { VirtualizeWindow } from '@/components/layout/virtualize';
 import { QueryClient, QueryClientProvider, useInfiniteQuery } from '@tanstack/react-query';
-import { MOCK_LIMIT, MOCK_LOAD_DELAY } from '@/utils/mock';
+import { MOCK_LIMIT } from '@/utils/mock';
+import { fetch_server_page } from '@/components/debug/list-load/fetch';
 
 const page_size = MOCK_LIMIT;
-const load_delay = MOCK_LOAD_DELAY;
-const total_rows = 200;
-
 const queryClient = new QueryClient();
-
-interface fetchResult {
-  rows: Array<string>;
-  nextOffset?: number;
-}
-async function fetchServerPage(limit: number, offset: number = 0): Promise<fetchResult> {
-  if (offset * limit >= total_rows) return { rows: [], nextOffset: undefined };
-  const rows = new Array(limit).fill(0).map((_, i) => `async loaded row #${i + offset * limit}`);
-  await new Promise((r) => setTimeout(r, load_delay));
-  return { rows, nextOffset: offset + 1 };
-}
 
 export const ListLoadContent = () => {
   const { data, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } = useInfiniteQuery({
     queryKey: ['list-load'],
-    queryFn: (ctx) => fetchServerPage(page_size, ctx.pageParam),
+    queryFn: (ctx) => fetch_server_page(page_size, ctx.pageParam),
     getNextPageParam: (lastGroup) => lastGroup.nextOffset,
     initialPageParam: 0,
   });
@@ -37,11 +24,10 @@ export const ListLoadContent = () => {
       hasNextPage={hasNextPage}
       fetchNextPage={fetchNextPage}
       overscan={page_size}
-      // renderRow={(item: any) => <div>{JSON.stringify(item)}</div>}
+      renderRow={(item: any) => <div className="py-1">{JSON.stringify(item)}</div>}
     />
   );
 };
-
 export const ListLoad = () => (
   <QueryClientProvider client={queryClient}>
     <ListLoadContent />
